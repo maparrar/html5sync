89351ac3c9241164b8ecf0fa89b72e4a9805f7a7
maparrar <maparrar@gmail.com>
2014-02-18 18:57:05 -0500

    Primeras pruebas de almacenamiento

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Herramienta para sincronizar una base de datos del servidor con una en el client
 Esta herramienta está en proceso de construcción, no hay ningún compromiso de que funcione.
 
 
+Referencia
+=========
+
+html5sync pretende ser compatible con varios tipos de almacenamiento en el navegador web. Debido a que la base de datos más robusta en el momento para HTML5 es indexedDB, será la primera en ser implementada. Para más información, consulte los siguientes enlaces:
+
+IndexedDB:
+* Conceptos básicos: https://developer.mozilla.org/en-US/docs/IndexedDB/Basic_Concepts_Behind_IndexedDB
+* Usando IndexedDB: https://developer.mozilla.org/en-US/docs/IndexedDB/Using_IndexedDB
+
+
 Changelog
 =========
 
--- index.php
+++ index.php
@@ -6,3 +6,114 @@
  * and open the template in the editor.
  */
 
+
+echo("Pruebas");
+
+?>
+<html>
+    <head>
+        <title>html5sync</title>
+        
+        <script src="scripts/jquery-2.1.0.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="scripts/base.css">
+        
+        <script type="text/javascript">
+            $( document ).ready(function(){
+                if(window["indexedDB"] === undefined) {
+                    console.log("Este navegador no soporta indexDB");
+                }else{
+                    debug("indexDB soportado...");
+                    
+                    debug("indexDB soportado...ssssssssssssss");
+                    
+                    console.debug(Storage);
+                }
+
+
+
+
+
+
+
+
+
+
+                
+                
+                
+                
+                
+                
+                
+                var db;
+                var request = indexedDB.open("html5sync");
+
+                request.onupgradeneeded = function() {
+                    // The database did not previously exist, so create object stores and indexes.
+                    db = request.result;
+                    var store = db.createObjectStore("music", {keyPath: "id"});
+                    var songIndex = store.createIndex("by_song", "song", {unique: true});
+                    var interpreterIndex = store.createIndex("by_interpreter", "interpreter");
+                    var albumIndex = store.createIndex("by_album", "album");
+
+                    // Populate with initial data.
+                    store.put({id: 1, interpreter: "Tom Yorke", song: "Analyse", album: "The eraser"});
+                    store.put({id: 2, interpreter: "Bob Marly", song: "One love", album: "Legend"});
+                    store.put({id: 3, interpreter: "Alice in Chains", song: "Angry Chair", album: "Unplugged"});
+                    
+                    
+                    
+                    var tx = db.transaction("music", "readwrite");
+                    var store = tx.objectStore("music");
+
+                    store.put({id: 4, interpreter: "Fito Paez", song: "Circo Beat", album: "Circo Beat"});
+                    store.put({id: 5, interpreter: "Urge Overkill", song: "Girl you'll be a woman soon", album: "Stull"});
+
+                    tx.oncomplete = function() {
+                      // All requests have succeeded and the transaction has committed.
+                    };
+                };
+
+                request.onsuccess = function() {
+                    db = request.result;
+                };
+                
+                db.onerror = function(event) {
+                    // Generic error handler for all errors targeted at this database's
+                    // requests!
+                    alert("Database error: " + event.target.errorCode);
+                };
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                function debug(message){
+                    $("#debug").append(message+"<br>");
+                }
+                
+            });
+        </script>
+    </head>
+    <body>
+        <section id="debug"></section>
+    </body>
+</html>
