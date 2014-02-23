ed6bed158518098114f92294075b53f9d7d45f19
maparrar <maparrar@gmail.com>
2014-02-23 13:29:44 -0500

    Nuevo archivo de configuración

---
+++ server/config.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * html5sync Plugin v.0.0.3 (https://github.com/maparrar/html5sync)
+ * Feb 2014
+ * - maparrar: http://maparrar.github.io
+ * - jomejia: https://github.com/jomejia
+ */
+/**
+ * Variables de configuración de html5sync
+ */
+return array(
+    /**
+     * Configuración de la Base de datos a usar
+     */
+    $database = array(
+        "database" => "musical",
+        "driver" => "mysql",
+        "host" => "localhost",
+        "user" => "html5sync",
+        "password" => "H5FAHM98hBS8"
+    )
+);
